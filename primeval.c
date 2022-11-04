3ca69b2672ffc9b59a4382780ce6552ce6e71641
Dylan Thinnes <dylan.thinnes@protonmail.com>
2022-11-04 18:31:53 +0000

    multithreading!

--- primeval.c
+++ primeval.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <pthread.h>
 #include "primes.h"
 
 // Simple sieve for prime numbers, bounded at 15485863 ^ 2
@@ -16,9 +17,9 @@ int is_prime (int n) {
 }
 
 int len = 1;
-int tallies[10] = {0,1,0,0,0,0,0,0,0,0};
-int prime_total = 0;
-int permutation_lengths[20];
+int global_idx = 0;
+int global_tally[10] = {0,1,0,0,0,0,0,0,0,0};
+pthread_mutex_t global_tally_mutex;
 
 long int hardcoded[21] =
       { 1
@@ -48,193 +49,162 @@ long int factorial (int n) {
   return hardcoded[n];
 }
 
-int estimates_made = 0;
-long int estimate_permutations (long int dedupe_count, int digits_so_far, int start_idx) {
-  long int total = 0;
-  if (start_idx == 10) {
-    total = factorial(digits_so_far) / dedupe_count;
-    //printf("Running total: %ld\n", total);
-    estimates_made += 1;
-    return total;
-  }
-  total += estimate_permutations(dedupe_count, digits_so_far, start_idx + 1);
-  for (int ii = 1; ii <= tallies[start_idx]; ii++) {
-    dedupe_count *= ii; // incrementally calculate factorial
-    digits_so_far += 1;
-    total += estimate_permutations(dedupe_count, digits_so_far, start_idx + 1);
-  }
-  return total;
-}
-
-int permute (int length, int is_first, int total, int power, int called_with_zero, int running_total) {
+int permute (int tally[10], int* prime_total, int length, int is_first, int total, int power, int called_with_zero, int running_total) {
   int tried = 0;
   int anything_emitted = 0;
 
   int tally_decrement = 1;
-  if (is_first) tally_decrement = 2;
+  //if (is_first) tally_decrement = 2;
 
   for (int ii = 9; ii >= 0; ii -= tally_decrement) {
-    if (is_first && ii == 5) continue;
-    if (tallies[ii]) {
+    //if (is_first && ii == 5) continue;
+    if (tally[ii]) {
       if (anything_emitted == 0 && ii == 0) break;
       anything_emitted = 1;
-      tallies[ii]--; //emit
+      tally[ii]--; //emit
       total += power * ii;
-      tried += permute(length + 1, 0, total, power * 10, 0 == ii, running_total + ii);
-      tallies[ii]++; //deemit
+      tried += permute(tally, prime_total, length + 1, 0, total, power * 10, 0 == ii, running_total + ii);
+      tally[ii]++; //deemit
       total -= power * ii;
     }
   }
 
-  if (!called_with_zero && running_total % 3 != 0) {
+  if (!called_with_zero /*&& running_total % 3 != 0*/) {
     tried += 1;
-    permutation_lengths[length]++;
-    if (length > 0 && is_prime(total)) {
-      prime_total++;
+    int prime = is_prime(total);
+    if (length > 0 && prime) {
+      *prime_total += 1;
     }
+    //printf("try %d %d\n", total, prime);
   }
 
   return tried;
 }
 
-int get_prime_total () {
-  prime_total = 0;
-  for (int ii = 0; ii < 20; ii++) permutation_lengths[ii] = 0;
-  int tried = permute(0, 1, 0, 1, 1, 0);
-  for (int ii = 0; ii < 20; ii++) printf("%d %d\n", ii, permutation_lengths[ii]);
-  return tried;
+void save_global_tally_to (int tally[10]) {
+  for (int ii = 0; ii < 10; ii++) {
+    tally[ii] = global_tally[ii];
+  }
 }
 
-void next_tally () {
-  if (tallies[9] == len) { // a
+void next_tally_global () {
+  global_idx++;
+  if (global_tally[9] == len) { // a
     len += 1;
-    tallies[0] = tallies[9];
-    tallies[1] = 1;
-    tallies[9] = 0;
+    global_tally[0] = global_tally[9];
+    global_tally[1] = 1;
+    global_tally[9] = 0;
     return;
-  } else if (tallies[0] == 0 && tallies[9] + 1 == len) { // b
-    // We know that tallies[1:8] contains a single value (i.e. is onehot)
+  } else if (global_tally[0] == 0 && global_tally[9] + 1 == len) { // b
+    // We know that global_tally[1:8] contains a single value (i.e. is onehot)
     for (int ii = 8; ii >= 1; ii--) {
-      if (tallies[ii]) {
-        tallies[ii]--;
-        tallies[0] = tallies[9];
-        tallies[9] = 0;
-        tallies[ii+1]++;
+      if (global_tally[ii]) {
+        global_tally[ii]--;
+        global_tally[0] = global_tally[9];
+        global_tally[9] = 0;
+        global_tally[ii+1]++;
         break;
       }
     }
-  } else if (tallies[0] > 0 && tallies[0] == len) { // e
+  } else if (global_tally[0] > 0 && global_tally[0] == len) { // e
     printf("Invalid case found with all zeroes!\n");
     exit(1);
-  } else if (tallies[0] > 0 && tallies[9] > 0 && tallies[0] + tallies[9] == len) { // f
-    tallies[0]--;
-    tallies[9]++;
-  } else if (tallies[0] > 0 && tallies[0] + tallies[9] + 1 == len) { // d
-    tallies[0]--;
-    // We know that tallies[1:8] contains a single value (i.e. is onehot)
+  } else if (global_tally[0] > 0 && global_tally[9] > 0 && global_tally[0] + global_tally[9] == len) { // f
+    global_tally[0]--;
+    global_tally[9]++;
+  } else if (global_tally[0] > 0 && global_tally[0] + global_tally[9] + 1 == len) { // d
+    global_tally[0]--;
+    // We know that global_tally[1:8] contains a single value (i.e. is onehot)
     for (int ii = 8; ii >= 1; ii--) {
-      if (tallies[ii]) {
-        tallies[ii] = tallies[9] + 2;
+      if (global_tally[ii]) {
+        global_tally[ii] = global_tally[9] + 2;
         break;
       }
     }
-    tallies[9] = 0;
+    global_tally[9] = 0;
   } else { // c
     for (int ii = 8; ii >= 1; ii--) {
-      if (tallies[ii]) {
-        tallies[ii]--;
-        int nines = tallies[9];
-        tallies[9] = 0;
-        tallies[ii+1] += 1 + nines;
+      if (global_tally[ii]) {
+        global_tally[ii]--;
+        int nines = global_tally[9];
+        global_tally[9] = 0;
+        global_tally[ii+1] += 1 + nines;
         break;
       }
     }
   }
 }
 
-void print_seq () {
-  printf(" %d:", len);
-  for (int ii = 0; ii < 10; ii++) {
-    printf(" %d", tallies[ii]);
-  }
-
-  printf(" : ");
-
+void print_tally (int tally[10]) {
   int first_nonzero = 0;
   for (int ii = 1; ii < 10; ii++) {
-    if (tallies[ii] > 0) {
+    if (tally[ii] > 0) {
       printf("%d", ii);
       first_nonzero = ii;
-      tallies[ii]--;
+      tally[ii]--;
       break;
     }
   }
-  for (int ii = 0; ii < tallies[0]; ii++) {
+  for (int ii = 0; ii < tally[0]; ii++) {
     printf("0");
   }
   for (int ii = 1; ii < 10; ii++) {
-    for (int jj = 0; jj < tallies[ii]; jj++) {
+    for (int jj = 0; jj < tally[ii]; jj++) {
       printf("%d", ii);
     }
   }
   if (first_nonzero > 0) {
-    tallies[first_nonzero]++;
+    tally[first_nonzero]++;
   }
 }
 
-int main () {
-  int best_so_far = -1;
-  //int nth_tally = 0;
-  int nth_tally = 0;
-  tallies[0] = 1;
-  tallies[1] = 2;
-  tallies[2] = 1;
-  tallies[3] = 1;
-  tallies[4] = 1;
-  tallies[5] = 1;
-  tallies[6] = 1;
-  tallies[7] = 1;
-  tallies[8] = 0;
-  tallies[9] = 1;
-  len = 10;
+void* start_primevals_thread (void* inp) {
+  int local_tally[10] = {0,0,0,0,0,0,0,0,0,0};
+  int local_idx = 0;
 
-  int skips_made = 0;
-  int total_tried = 0;
-  int total_skippable = 0;
-  int ii = 100;
-  while (ii--) {
-    estimates_made = 0;
-    long int estimated_perms = estimate_permutations(1, 0, 0);
-    printf("permutation estimate: %ld %d\n", estimated_perms, estimates_made);
+  while (1) {
+    // Get next tally
+    pthread_mutex_lock(&global_tally_mutex);
+    save_global_tally_to(local_tally);
+    local_idx = global_idx;
+    next_tally_global();
+    pthread_mutex_unlock(&global_tally_mutex);
 
-    if (estimated_perms >= best_so_far) {
-      fprintf(stdout, "Getting prime total...\n");
-      int tried = get_prime_total();
-      total_tried += tried;
-      if (tried < best_so_far) total_skippable += tried;
-      fprintf(stdout, "Tried %d, got new prime total: %d\n", tried, prime_total);
-      if (prime_total > best_so_far) {
-        best_so_far = prime_total;
-        printf("Improvement found!\n");
-        print_seq();
-        printf(": %d : %d\n", nth_tally, prime_total);
-      } else {
-        print_seq();
-        printf("\n");
-      }
-    } else {
-      skips_made++;
-      printf("Skipping, permutation estimate too low.");
-    }
-    //break;
-
-    fprintf(stdout, "Sourcing new tally...\n");
-    next_tally();
-    fprintf(stdout, "Sourced new tally.\n");
-    nth_tally++;
+    int prime_total = 0;
+    //printf("Testing ");
+    //print_tally(local_tally);
+    //printf("\n");
+    int tried = permute(local_tally, &prime_total, 0, 1, 0, 1, 1, 0);
+    //printf("Report ");
+    print_tally(local_tally);
+    printf(" %d %d %d\n", local_idx, tried, prime_total);
   }
 
-  printf("Skips made: %d\n", skips_made);
-  printf("Total tried: %d\n", total_tried);
-  printf("Total skippable: %d\n", total_skippable);
+  return NULL;
+}
+
+#define NUM_THREADS 8
+
+int main () {
+  global_idx = 0;
+  global_tally[0] = 1;
+  global_tally[1] = 2;
+  global_tally[2] = 1;
+  global_tally[3] = 1;
+  global_tally[4] = 1;
+  global_tally[5] = 1;
+  global_tally[6] = 1;
+  global_tally[7] = 1;
+  global_tally[8] = 0;
+  global_tally[9] = 1;
+  len = 10;
+
+  pthread_t threads[NUM_THREADS];
+  for (int ii = 0; ii < NUM_THREADS; ii++) {
+    pthread_create(&threads[ii], NULL, start_primevals_thread, NULL);
+  }
+
+  for (int ii = 0; ii < NUM_THREADS; ii++) {
+    pthread_join(threads[ii], NULL);
+  }
 }
